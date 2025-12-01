49fed34f61a88ba1a9f2b1092aa1e9cb7a083d62
theaux42 <tbabou@student.42.fr>
2025-12-01 13:43:56 +0100

    :lock: Blocked access to mariadb without password as root

--- srcs/requirements/mariadb/tools/create_database.sh
+++ srcs/requirements/mariadb/tools/create_database.sh
@@ -21,9 +21,15 @@ echo "Database: ${MYSQL_DATABASE}"
 echo "User: ${MYSQL_USER}"
 
 cat > /tmp/db1.sql << EOF
+# 1. Définir la base de données et l'utilisateur de l'application (votre code)
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+
+# Force root@localhost à utiliser le mot de passe défini par MARIADB_ROOT_PASSWORD
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'
+    REPLACE 'unix_socket' WITH 'mysql_native_password';
+
 FLUSH PRIVILEGES;
 EOF
 
